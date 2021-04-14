8089e34017dbb40b9d619dbd16d1a45965d9a92b
CastroOlympias <blackcarrera@msn.com>
2021-04-13 18:58:13 -0600

    <have added the ability to search repos, and dispary who's repositors have been search only>

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,8 +1,11 @@
-fetch("https://api.github.com/users/octocat/repos").then(function(response) {
-  response.json().then(function(data) {
-    console.log(data);
-  });
-});
+// fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+//   response.json().then(function(data) {
+//     console.log(data);
+//   });
+// });
+
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
 
 var getUserRepos = function(user) {
     // format the github api url
@@ -11,7 +14,35 @@ var getUserRepos = function(user) {
     // make a request to the url
     fetch(apiUrl).then(function(response) {
       response.json().then(function(data) {
+        displayRepos(data, user);
         console.log(data);
       });
     });
   };
+
+  var userFormEl = document.querySelector("#user-form");
+  var nameInputEl = document.querySelector("#username");
+
+  var formSubmitHandler = function(event) {
+    event.preventDefault();
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+    console.log(event);
+  };
+
+var displayRepos = function(repos, searchTerm) {
+    // clear old content
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+    console.log(repos);
+    console.log(searchTerm);
+};
+
+  userFormEl.addEventListener("submit", formSubmitHandler);
